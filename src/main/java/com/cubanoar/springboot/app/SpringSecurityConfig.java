88e3f3bb03468f4a52824750ce6cc8a81ee7d663
cubanoar <dasiel.ramirez@yahoo.com>
2022-02-12 11:46:51 -0300

    Agregando OpenPDF

--- src/main/java/com/cubanoar/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/cubanoar/springboot/app/SpringSecurityConfig.java
@@ -1,17 +1,13 @@
 package com.cubanoar.springboot.app;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.cubanoar.springboot.app.auth.handler.LoginSuccessHandler;
 import com.cubanoar.springboot.app.models.service.JpaUserDetailsService;
