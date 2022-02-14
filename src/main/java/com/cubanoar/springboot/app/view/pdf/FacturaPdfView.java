915c02464d40a2ce9d028dbaba7197b995b1c41b
cubanoar <dasiel.ramirez@yahoo.com>
2022-02-14 00:03:58 -0300

    Final OpenPdf

--- src/main/java/com/cubanoar/springboot/app/view/pdf/FacturaPdfView.java
+++ src/main/java/com/cubanoar/springboot/app/view/pdf/FacturaPdfView.java
@@ -1,5 +1,6 @@
 package com.cubanoar.springboot.app.view.pdf;
 
+import java.awt.Color;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,9 +10,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.cubanoar.springboot.app.models.entity.Factura;
+import com.cubanoar.springboot.app.models.entity.ItemFactura;
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
+
 
 @Component("factura/ver")
 public class FacturaPdfView extends AbstractPdfView{
@@ -24,22 +29,60 @@ public class FacturaPdfView extends AbstractPdfView{
 		
 		//Creando las tablas
 		PdfPTable tabla = new PdfPTable(1);//le pasamos la cantidad de columnas
-		tabla.setSpacingAfter(20f);//Para dar espacio despues de la tabla
-		tabla.addCell("Datos del cliente");
+		
+		PdfPCell cell = null;
+		cell = new PdfPCell(new Phrase("Datos del cliente"));
+		cell.setBackgroundColor(new Color(184,218,255));
+		cell.setPadding(6);
+		tabla.addCell(cell);
+		
 		tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
 		tabla.addCell(factura.getCliente().getEmail());
+		tabla.setSpacingAfter(20);//Para dar espacio despues de la tabla
+		
+		document.add(tabla);
 		
 		PdfPTable tabla2 = new PdfPTable(1);
+		cell = new PdfPCell(new Phrase("Datos de la Factura"));
+		cell.setBackgroundColor(new Color(195,230,203));
+		cell.setPadding(6);
+		tabla2.addCell(cell);
+		
+		tabla2.addCell("Folio: "  + factura.getId());
+		tabla2.addCell("Decripición: " + factura.getDescripcion());
+		tabla2.addCell("Fecha: " + factura.getCreateAt());
 		tabla2.setSpacingAfter(20);
-		tabla.addCell("Datos de la Factura");
-		tabla.addCell("Folio: "  + factura.getId());
-		tabla.addCell("Decripición: " + factura.getDescripcion());
-		tabla.addCell("Fecha: " + factura.getCreateAt());
 		
 		//Añadiendo las tablas
-		document.add(tabla);
 		document.add(tabla2);
 		
+		PdfPTable tabla3 = new PdfPTable(4);
+		//tabla3.setWidthPercentage(100);
+		/*le pasamos las medidas de cada columna*/
+		tabla3.setWidths(new float [] {3f, 1, 1, 1});
+		tabla3.addCell("Producto");
+		tabla3.addCell("Precio");
+		tabla3.addCell("Cantidad");
+		tabla3.addCell("Total");
+		
+		for (ItemFactura item : factura.getItems()) {
+			tabla3.addCell(item.getProducto().getNombre());
+			tabla3.addCell(item.getProducto().getPrecio().toString());
+			
+			cell = new PdfPCell(new Phrase(item.getCantidad().toString()));
+			cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+			
+			tabla3.addCell(cell);
+			tabla3.addCell(item.calcularImporte().toString());
+		}
+		
+		cell = new PdfPCell(new Phrase("Total: "));
+		cell.setColspan(3);
+		cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+		tabla3.addCell(cell);
+		tabla3.addCell(factura.getTotal().toString());
+		
+		document.add(tabla3);
 		
 	}
 
